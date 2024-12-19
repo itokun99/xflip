076f05ef3d8eed7a127a94015172e4862a0340d4
itokun99 <indrawanlisantopersonal@gmail.com>
2024-12-20 06:02:47 +0700

    adjust query libs

--- src/core/libs/query/components.tsx
+++ src/core/libs/query/components.tsx
@@ -1,11 +1,24 @@
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import React, { PropsWithChildren } from "react";
-import { appQueryClient } from "./config";
+import { appQueryClient, asyncStoragePersister } from "./config";
 
 export const AppQueryClientProvider = ({ children }: PropsWithChildren) => {
   return (
     <QueryClientProvider client={appQueryClient}>
       {children}
     </QueryClientProvider>
+  );
+};
+
+export const AppPersistQueryClientProvider = ({
+  children,
+}: PropsWithChildren) => {
+  return (
+    <PersistQueryClientProvider
+      client={appQueryClient}
+      persistOptions={{ persister: asyncStoragePersister }}>
+      {children}
+    </PersistQueryClientProvider>
   );
 };
--- src/core/libs/query/config.ts
+++ src/core/libs/query/config.ts
@@ -1,12 +1,24 @@
-import { QueryClient } from "@tanstack/react-query";
+import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
+import { QUERY_CONFIG } from "@core/configs";
+import { RQ } from "@core/packages";
+import { asyncStorage } from "../storage";
 
-export const appQueryClient = new QueryClient({
+export const appQueryClient = new RQ.QueryClient({
   defaultOptions: {
     queries: {
-      retry: 0,
+      retry: QUERY_CONFIG.retry,
+      gcTime: QUERY_CONFIG.gcTime,
     },
     mutations: {
-      retry: 0,
+      retry: QUERY_CONFIG.retry,
     },
   },
+});
+
+export const asyncStoragePersister = createAsyncStoragePersister({
+  key: QUERY_CONFIG.persistorKey,
+  storage: asyncStorage,
+  throttleTime: QUERY_CONFIG.persistorThrottleTime,
+  serialize: JSON.stringify,
+  deserialize: JSON.parse,
 });
