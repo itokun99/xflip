81c69f5ebc9ecf2c6ac37eeda6b6ad5cf1a9dc6a
itokun99 <indrawanlisantopersonal@gmail.com>
2024-12-20 06:02:26 +0700

    adjust language libs

--- src/core/libs/language/utils.ts
+++ src/core/libs/language/utils.ts
@@ -1,14 +1,3 @@
-import { dicts } from "./dicts";
-import { defaultLanguage, languageAtom, languageStore } from ".";
-
-type DictionaryKeys = keyof typeof dicts;
-
-export const getDictionary = (word: DictionaryKeys) => {
-  const userLang = languageStore.get(languageAtom);
-  if (!word) return "";
-  return dicts?.[word]?.[userLang] || dicts?.[word]?.[defaultLanguage] || word;
-};
-
 export function replaceString(
   template: string,
   replacements: Record<string, string>,
