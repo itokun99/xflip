eb7a28bbbe0a41dce1f604b236fcefe51e410f46
itokun99 <indrawanlisantopersonal@gmail.com>
2024-12-20 14:40:58 +0700

    adjust base card styling with memo

--- src/features/_global/components/cards/base-card.tsx
+++ src/features/_global/components/cards/base-card.tsx
@@ -1,25 +1,38 @@
 import { StyleProp, StyleSheet, View, ViewStyle } from "@core/packages";
 import { rounded, spacings } from "@core/styles";
 import { useColors } from "@features/_global";
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useMemo } from "react";
+import { Pressable } from "react-native-gesture-handler";
 
 export interface BaseCardProps extends PropsWithChildren {
   style?: StyleProp<ViewStyle>;
+  onPress?: () => void;
 }
 
 export const BaseCard = React.memo((props: BaseCardProps) => {
   const colors = useColors();
-  return (
-    <View
-      style={[
+  const Wrapper = useMemo(
+    () => (typeof props.onPress === "function" ? Pressable : View),
+    [props.onPress],
+  );
+
+  const wrapperStyle = useMemo(
+    () =>
+      StyleSheet.flatten([
+        {
+          borderTopColor: colors.black(0.1),
+          borderLeftColor: colors.black(0.1),
+          borderRightColor: colors.black(0.1),
+          borderBottomColor: colors.black(0.1),
+        },
         styles.container,
         colors.bgWhiteAlpha100,
-        { borderColor: colors.black(0.1) },
         props.style,
-      ]}>
-      {props.children}
-    </View>
+      ]),
+    [colors, props.style],
   );
+
+  return <Wrapper style={wrapperStyle}>{props.children}</Wrapper>;
 });
 
 const styles = StyleSheet.create({
