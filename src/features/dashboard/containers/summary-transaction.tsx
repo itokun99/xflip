efbd4684250f71725326c64e5efef317bfb2ec93
itokun99 <indrawanlisantopersonal@gmail.com>
2024-12-20 10:23:56 +0700

    create summary transaction for dashboard

---
+++ src/features/dashboard/containers/summary-transaction.tsx
@@ -0,0 +1,77 @@
+import { useLanguage } from "@core/libs";
+import { View, StyleSheet, Pressable } from "@core/packages";
+import { appStyles } from "@core/styles";
+import {
+  BaseCard,
+  P,
+  SectionLayout,
+  SvgIcon,
+  useColors,
+} from "@features/_global";
+import { useTransaction } from "@features/transaction";
+import { ArrowRight, Check, CheckCircle, Clock } from "lucide-react-native";
+import React from "react";
+
+export const SummaryTransaction = React.memo(() => {
+  const transaction = useTransaction();
+  const language = useLanguage();
+  const colors = useColors();
+
+  return (
+    <SectionLayout header={false} animateDuration={1000}>
+      <View style={[appStyles.gmd, appStyles.flexColumn]}>
+        <BaseCard style={[appStyles.flex1]}>
+          <View style={[appStyles.flexRow, appStyles.gxxs]}>
+            <SvgIcon
+              name="Banknote"
+              size={14}
+              style={[appStyles.mtxxs]}
+              color={colors.success(1)}
+            />
+            <P>{language.dictionary("totalAmount")}</P>
+          </View>
+          <P size="xl" bold>
+            {transaction.totalAmountFormatted}
+          </P>
+        </BaseCard>
+        <View style={[appStyles.flexRow, appStyles.gmd]}>
+          <BaseCard style={[appStyles.flex1]}>
+            <View style={[appStyles.flexRow, appStyles.gxxs]}>
+              <CheckCircle
+                size={14}
+                style={[appStyles.mtxxs]}
+                color={colors.success(1)}
+              />
+              <P size="sm">{language.dictionary("success")}</P>
+            </View>
+            <P size="sm" bold>
+              {Number(transaction.success?.length)}
+            </P>
+          </BaseCard>
+          <BaseCard style={[appStyles.flex1]}>
+            <View style={[appStyles.flexRow, appStyles.gxxs]}>
+              <SvgIcon
+                name="Clock"
+                size={14}
+                style={[appStyles.mtxxs]}
+                color={colors.warning(1)}
+              />
+              <P size="sm">{language.dictionary("pending")}</P>
+            </View>
+            <P size="sm" bold>
+              {Number(transaction.pending?.length)}
+            </P>
+          </BaseCard>
+        </View>
+        <BaseCard style={[{ flex: -1 }]}>
+          <Pressable style={[appStyles.flexRow, appStyles.justifyBetween]}>
+            <P size="sm">{language.dictionary("seeAllTransaction")}</P>
+            <ArrowRight color={colors.primary(1)} size={24} />
+          </Pressable>
+        </BaseCard>
+      </View>
+    </SectionLayout>
+  );
+});
+
+const styles = StyleSheet.create({});
--- src/features/dashboard/pages/dashboard.tsx
+++ src/features/dashboard/pages/dashboard.tsx
@@ -1,12 +1,20 @@
-import { React } from "@core/packages";
+import { React, View } from "@core/packages";
 import { appStyles } from "@core/styles";
 import { DashboardLayout } from "../components";
+import { useTransaction } from "@features/transaction";
+import { useLanguage } from "@core/libs";
+import { SummaryTransaction } from "../containers";
 
 export const Dashboard = React.memo(() => {
+  const transaction = useTransaction();
   return (
     <DashboardLayout
       bgMode="light"
-      containerStyle={[appStyles.ptmd]}></DashboardLayout>
+      refreshing={transaction.isLoading}
+      onRefresh={transaction.refetch}
+      containerStyle={[appStyles.ptmd, appStyles.phmd]}>
+      <SummaryTransaction />
+    </DashboardLayout>
   );
 });
 
