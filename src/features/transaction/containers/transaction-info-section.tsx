b06ad09269056dbe22325c59398e417959efff50
itokun99 <indrawanlisantopersonal@gmail.com>
2024-12-21 05:51:23 +0700

    update transaction detail card

--- src/features/transaction/containers/transaction-info-section.tsx
+++ src/features/transaction/containers/transaction-info-section.tsx
@@ -1,14 +1,17 @@
 import { TransactionList } from "../components";
 import { useTransaction, useTransactionDetail } from "..";
 import React from "react";
-import { BaseCard, P } from "@features/_global";
+import { BaseCard, P, SvgIcon, useColors, useStyles } from "@features/_global";
 import { View } from "@core/packages";
 import { appStyles } from "@core/styles";
 import { useLanguage } from "@core/libs";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export const TranscationInfoSection = React.memo(() => {
   const language = useLanguage();
   const detail = useTransactionDetail();
+
+  const colors = useColors();
 
   if (!detail)
     return (
@@ -23,10 +26,13 @@ export const TranscationInfoSection = React.memo(() => {
         <BaseCard>
           <View>
             <P>ID Transsaksi</P>
-            <View>
+            <View style={[appStyles.flexRow, appStyles.justifyBetween]}>
               <P size="xl" bold>
                 {detail?.id ? `#${detail?.id}` : ""}
               </P>
+              <TouchableOpacity>
+                <SvgIcon name="Copy" size={24} color={colors.primary(1)} />
+              </TouchableOpacity>
             </View>
           </View>
         </BaseCard>
