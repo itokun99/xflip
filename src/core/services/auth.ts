96a5a581244b1677d5a37fdf03f39b59df1c8cd7
itokun99 <indrawanlisantopersonal@gmail.com>
2024-12-20 10:20:27 +0700

    remove unused auth code

--- src/core/services/auth.ts
+++
@@ -1,6 +0,0 @@
-import { LoginRequestModel, RegisterRequestModel } from "@core/models/auth";
-
-export const authService = {
-  login: async (payload: LoginRequestModel) => {},
-  register: async (payload: RegisterRequestModel) => {},
-};
