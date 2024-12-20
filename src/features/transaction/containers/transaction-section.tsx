890a796542b1f2df0b050adaf51c7d5552a9530a
itokun99 <indrawanlisantopersonal@gmail.com>
2024-12-20 22:12:04 +0700

    adjust transaction section container

--- src/features/transaction/containers/transaction-section.tsx
+++ src/features/transaction/containers/transaction-section.tsx
@@ -1,11 +1,17 @@
 import { TransactionList } from "../components";
 import { useTransaction } from "..";
 import React from "react";
+import { SearchFilter } from "@features/_global";
+import { View } from "@core/packages";
+import { appStyles } from "@core/styles";
 
 export const TransactionSection = React.memo(() => {
   const transaction = useTransaction();
   return (
     <>
+      <View style={[appStyles.phmd, appStyles.pvmd]}>
+        <SearchFilter />
+      </View>
       <TransactionList
         items={transaction.all || []}
         refreshing={transaction.isLoading}
